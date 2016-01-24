c2a9c4a9c25b70f95056a9981326192b7e2a5776
Zlatko Yankov <zlatkoyankov@gmail.com>
2016-01-24 23:48:28 +0200

    everything work with commits if you commit correct file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,24 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var jshint = require('gulp-jshint');
 
-gulp.task('default', function(){
-	console.log('some test that gulp worl');
+gulp.task('lint', function() {
+	return gulp.src('js/*.js')
+	  .pipe(jshint())
+		.pipe(jshing.reported('default'));
 });
+
+
+gulp.task('sass',function() {
+	return gulp.src('sass/*.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('css'));
+});
+
+
+gulp.task('hello', function() {
+	console.log('hello from gulp')
+});
+
+//add some text but git ignore to update this file
+//https://travismaynard.com/writing/getting-started-with-gulp
